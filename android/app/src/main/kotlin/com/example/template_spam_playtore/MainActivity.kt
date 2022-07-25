54ab3a8ee7c6355d517906bfeb4f5c5e5ade36a9
novrizaledward <novrizaledward@gmail.com>
2022-07-25 10:24:00 +0700

    First Upload

---
+++ android/app/src/main/kotlin/com/example/template_spam_playtore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.template_spam_playtore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/pages/validation_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ValidationPage extends StatelessWidget {
+  const ValidationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
